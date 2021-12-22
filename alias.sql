24bd4c0e9fcb6adf25b835887267edd0c90b7031
soohan <tngks0315@naver.com>
2021-12-23 00:06:39 +0900

    2021223 update

--- alias.sql
+++ alias.sql
@@ -5,3 +5,13 @@ SELECT CONCAT_WS(" ", contactLastName, contactFirstName) AS contactFullName FROM
 
 -- MySQL은 아래와 같은 축약어도 사용이 가능하다.
 SELECT orderNumber `Order no.`, SUM(priceEach * quantityOrdered) total FROM orderdetails GROUP BY `Order no.` HAVING total > 60000;
+
+-- Table Alias
+SELECT 
+    customerName, COUNT(o.orderNumber) total
+FROM customers c
+INNER JOIN orders o ON c.customerNumber = o.customerNumber
+GROUP BY
+    customerName
+ORDER BY
+    total DESC;
