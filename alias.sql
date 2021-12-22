79a2dc73df5e3ea8f1953001ac79e2ffb6c0c579
soohan <tngks0315@naver.com>
2021-12-22 23:44:29 +0900

    20211222 update

--- alias.sql
+++ alias.sql
@@ -1,3 +1,6 @@
 -- 문자열 결합하기
+-- contactLastName과 concatFirstName을 공백으로 결합하여 하나의 열로 만들기 
 SELECT CONCAT_WS(" ", contactLastName, contactFirstName) FROM customers
+SELECT CONCAT_WS(" ", contactLastName, contactFirstName) AS contactFullName FROM customers
+
 
