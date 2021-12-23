8497fd5f67cf9072ceeaf74c5985a0800761f022
soohan <tngks0315@naver.com>
2021-12-23 18:29:19 +0900

    20211223 update

--- GROUPING.sql
+++ GROUPING.sql
@@ -1,4 +1,4 @@
--- GROUPING 시 ALIAS로는 GROUP BY가 되지 않는다.
+-- GROUPING 시 기본적으로 SELECT보다 GROUP BY가 먼저 실행되므로 ALIAS로는 GROUP BY가 되지 않는다.
 -- 배송이 완료된 주문에 대한 연도 별 합계총액 
 SELECT
     YEAR(orderDate) AS year,
@@ -12,3 +12,17 @@ WHERE
 GROUP BY
     YEAR(orderDate)
 
+-- HAVING은 그룹에 대한 Filtering을 적용한다. 
+SELECT
+    YEAR(orderDate) AS year,
+    SUM(quantityOrdered * priceEach) AS total
+FROM
+    orders
+INNER JOIN orderdetails
+    USING(orderNumber)
+WHERE
+    status = 'Shipped'
+GROUP BY
+    year
+HAVING
+    year > 2003;
